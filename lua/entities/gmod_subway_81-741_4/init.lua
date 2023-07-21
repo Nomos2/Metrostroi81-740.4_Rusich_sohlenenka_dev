ab1552bf5c5b43e820e22b8801347277df2fe034
Nomos2 <97733899+Nomos2@users.noreply.github.com>
2023-07-22 00:32:31 +0300

    Add files via upload
    Фикс перевода кнопок, фикс чоппера.

--- lua/entities/gmod_subway_81-741_4/init.lua
+++ lua/entities/gmod_subway_81-741_4/init.lua
@@ -753,7 +753,7 @@ end
     local state = math.abs(self.AsyncInverter.InverterFrequency/(11+self.AsyncInverter.State*5))--(10+8*math.Clamp((self.AsyncInverter.State-0.4)/0.4,0,1)))
     self:SetPackedRatio("asynccurrent", math.Clamp(state*(state+self.AsyncInverter.State/1),0,1)*math.Clamp(self.Speed/6,0,1))
     self:SetPackedRatio("asyncstate", math.Clamp(self.AsyncInverter.State/0.2*math.abs(self.AsyncInverter.Current)/100,0,1))
-    self:SetPackedRatio("chopper", math.Clamp(self.Electric.Chopper>0 and self.Electric.Chopper/100 or 0,0,1))	
+    self:SetPackedRatio("chopper", math.Clamp(self.Electric.Chopper>0 and self.Electric.IChopped/100 or 0,0,1))	 
 
 		--скорость дверей
 		for k,v in pairs(self.Pneumatic.LeftDoorSpeed) do
