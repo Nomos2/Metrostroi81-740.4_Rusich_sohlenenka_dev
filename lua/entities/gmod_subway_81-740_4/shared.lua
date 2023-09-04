a6e4778d562bfe284e54392a78972143e91d3977
Nomos2 <97733899+Nomos2@users.noreply.github.com>
2023-09-05 00:06:27 +0300

    Add files via upload
    Дополнение к торцевым дверям.

--- lua/entities/gmod_subway_81-740_4/shared.lua
+++ lua/entities/gmod_subway_81-740_4/shared.lua
@@ -519,6 +519,10 @@ ENT.Spawner = {
 				if not IsValid(ent) then return end				
 				ent:GetNW2Entity("gmod_subway_kuzov").RearDoor = val == 2
 				end)
+				timer.Simple(0,function()	
+				if not IsValid(ent) then return end				
+				ent:GetNW2Entity("gmod_subway_kuzov_741").RearDoor = val == 2
+				end)				
 				
                 ent.PassScheme:TriggerInput("Set",val==1 and 1 or 0)
 				--ent.BUKP.State = 0	
